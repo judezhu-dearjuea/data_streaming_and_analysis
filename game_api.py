3077b365eb0c68afc0154158627eeabcb97de688
Rathin Bector <rathin.bector@berkeley.edu>
2021-08-02 22:57:54 +0000

    Implemented initialize and purchase_weapon

--- game_api.py
+++ game_api.py
@@ -10,68 +10,76 @@ redis_store = redis.Redis(
     host='redis',
     port='6379')
 
+weapon_prices = {
+    "stick": 0,
+    "knife": 5,
+    "sword": 10,
+    "grenade": 15,
+    "gun": 50,
+    "bazooka": 100,
+    "nuke": 500
+}
 
 def log_to_kafka(topic, event):
     event.update(request.headers)
     producer.send(topic, json.dumps(event).encode())
 
-# tracker = {'wallet':100,'enemies':1,'weapon':[]}
-@app.route("/initialize",methods = ['GET','POST'])
-def initialize():
-    user_name = request.args.get('username',default='player',type=str)
-    
-    if redis.exists(user_name):
-        return "Username Taken! Please Input Another Username"
-    
-    else:
-        user = {"wallet":100,
-                "weapon":{"sword":0,
-                          "gun":0},
-                "health":10}
-        redis_store.hmset(user_name, user)
-        initial_event = {'event_type':'initiaize player',
-                         'username':user_name}
-        log_to_kafka('events',initial_event)
-    
-    return "Game Started!\n"
-
-    
 @app.route("/")
 def default_response():
     default_event = {'event_type': 'default'}
     log_to_kafka('events', default_event)
     return "This is the default response!\n"
+
+@app.route("/initialize",methods = ['GET','POST'])
+def initialize():
+    user_name = request.args.get('username',default='player',type=str)
     
-#@app.route("/purchase_a_sword")
-#def purchase_a_sword():
-#    purchase_sword_event = {'event_type': 'purchase_sword',
-#                            'weapon':'sword',
-#                            'cost':20,
-#                            'killing_power':0.7}
-#    log_to_kafka('events', purchase_sword_event)
-#    return "Sword Purchased!\n"
- 
-#Use this to call - n number of xxx weapon purchased
+    if redis_store.hexists("wallet",user_name):
+        return "Username Taken! Please Input Another Username. \n"
+    
+    else:
+        wallet = 100
+        health = 10
+        weapon = "stick"
+        redis_store.hset("wallet",user_name, wallet)
+        redis_store.hset("health",user_name, health)
+        redis_store.hset("weapon",user_name, weapon)
+        initial_event = {'event_type':'initiaize player',
+                         'username':user_name}
+        log_to_kafka('events',initial_event)
+        
+        return (
+            "Game Started!\nYou have %i in wallet, %i in health and a %s as a weapon. \n" % (wallet, health, weapon)
+        )
+    
 #docker-compose exec mids curl "http://localhost:5000/purchase_weapon?w=sword&n=3"    
 @app.route("/purchase_weapon",methods = ['GET','POST'])
 def purchase_weapon():
-    weapon_type = request.args.get('w',default='sword',type=str)
-    n = request.args.get('n',default=1,type=int)
-    purchase_weapon_event = {'event_type': 'purchase_weapon',
-                             'weapon_type': weapon_type,
-                             'n':n}
-    log_to_kafka('events', purchase_weapon_event)
-    return str(n) +" " + weapon_type + "(s) Purchased\n"
+    user_name = request.args.get('username',type=str)
+    weapon = request.args.get('weapon',default='stick',type=str)
+    
+    if not redis_store.hexists("wallet",user_name):
+        return "Not Valid Username! Please Initialize.\n"
+    elif weapon not in weapon_prices.keys():
+        return_string = ", ".join(str(x) for x in weapon_prices.keys())
+        return "Invalid Weapon! Please Input a Valid  Weapon from below.\n" + return_string + "\n"
+    elif int(redis_store.hget("wallet",user_name)) < weapon_prices[weapon]:
+        return "Not enough money in wallet! Get more to purchase weapon.\n"
+    else:
+        weapon_price = weapon_prices[weapon]
+        wallet_before = int(redis_store.hget("wallet", user_name))
+        wallet_after = wallet_before - weapon_price
+        redis_store.hset("wallet", user_name, wallet_after)
+        redis_store.hset("weapon", user_name, weapon)
+        purchase_weapon_event = {
+            'event_type': 'purchase_weapon',
+            'weapon': weapon,
+            'wallet_before': wallet_before,
+            'wallet_after': wallet_after
+        }
+        log_to_kafka('events', purchase_weapon_event)
+        return weapon + " Purchased!\n"
 
-#Use this to call - n number of players added
-#docker-compose exec mids curl "http://localhost:5000/add_player?n=5"    
-@app.route("/add_player", methods = ['GET','POST'])
-def add_player():
-    n = request.args.get('n',default=1,type=int)
-    add_player_event = {'event_type':'player_added',
-                        'n': n}
-    log_to_kafka('events',add_player_event)
-    return str(n) + " Player Added!\n"
 
 #Use this to call - kill a player using xxx weapon
 #docker-compose exec mids curl "http://localhost:5000/kill_player?w='sword'"    
